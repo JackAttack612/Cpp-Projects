e6dda82b6b50859917f0248d325bb958c08851a7
JackAttack612 <jetflyer1130@gmail.com>
2021-10-26 17:53:39 -0400

    Update

--- Projects/Account-Login/Account_Login.cpp
+++ Projects/Account-Login/Account_Login.cpp
@@ -1,13 +1,66 @@
 //Login or Register an Account (Using code from a video tutorial)
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+bool IsLoggedIn()
+{
+    string username, password, un, pw;
+
+    cout << "Enter Username: "; cin >> username;
+    cout << "Enter Password: "; cin >> password;
+
+    ifstream read("data\\" + username + ".txt");
+    getline(read, un);
+    getline(read, pw);
+
+    if (un == username && pw == password)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 int main()
 {
-    //Start of code
-    cout << "Login or Register" << endl;
+    int choice;
 
-    return 0;
+    cout << "1: Register\n2: Login\nChoose '1' or '2': "; cin >> choice;
+    if (choice == 1)
+    {
+        string username, password;
+
+        cout << "Select a username: "; cin >> username;
+        cout << "Select a password: "; cin >> password;
+
+        ofstream file;
+        file.open("data\\" + username + ".txt");
+        file << username << endl << password;
+        file.close();
+
+        main();
+    }
+    else if (choice == 2)
+    {
+        bool status = IsLoggedIn();
+
+        if (!status)
+        {
+            cout << "False Login!" << endl;
+            system("PAUSE");
+            return 0;
+        }
+        else
+        {
+            cout << "Succesfully logged in!" << endl;
+            system("PAUSE");
+            return 1;
+        }
+    }
 }
