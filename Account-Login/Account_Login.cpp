b200ddd73150872bf70e906ff3cbf67803b554f8
JackAttack612 <jetflyer1130@gmail.com>
2021-10-30 21:32:46 -0400

    Update

--- Account-Login/Account_Login.cpp
+++ Account-Login/Account_Login.cpp
@@ -50,18 +50,22 @@ int options()
                 int charAge;
                 string pronoun;
                 string continuation;
+                string gender;
 
 
                 cin.ignore();
                 cout << "\nWhat is your characters name: ";
                 getline(cin, charName);
-                cin.ignore();
+                
                 cout << "What is your characters age: ";
                 cin >> charAge;
                 cin.ignore();
-                cout << "What are your characters pronouns (He or She): ";
-                getline(cin, pronoun);
-                cout << endl << "There one was a " << pronoun << " named " << charName<< endl;
+                cout << "Pick a pronoun: ";
+                cin >> pronoun;
+                cin.ignore();
+                cout << "Pick a gender: ";
+                cin >> gender;
+                cout << endl << "There one was a " << gender << " named " << charName<< endl;
                 cout << pronoun << " was " << charAge << " years old" << endl;
                 cout << pronoun << " really liked the name " << charName << endl;
                 cout << "but didn't like being " << charAge << "." << endl;
