2b2e37840ecfd0a87433852ace6be2c7d5e297bd
JackAttack612 <jetflyer1130@gmail.com>
2021-12-12 22:59:37 -0500

    af

--- Raw-Files/Fornite.cpp
+++ Raw-Files/Fornite.cpp
@@ -4,8 +4,20 @@ using namespace std;
 
 int main()
 {
-    int a;
-    cout << "Running Fornite.."; 
-    cin >> a;
+    string Audrey = "Audrey is perfect.";
+    string HAU;
+    char yon;
+    cout << "Hello, I am Jackson. How are you? "; cin >> HAU;
+    cout << "Do you want to hear a fact? (Y or N): "; cin >> yon;
+
+    if (yon == 'n' | yon == 'N')
+    {
+        cout << Audrey;
+    }
+    else if (yon == 'Y' | yon == 'y')
+    {
+        cout << Audrey;
+    }
     return 0;
+
 }
